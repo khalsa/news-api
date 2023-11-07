172078265a0fabdd9b3b953e285d96142494d5ec
khalsa <saurabh.itbhu04@gmail.com>
2023-11-06 18:31:05 -0800

    Initail code commit

---
+++ src/main/java/com/demo/newsapi/encryption/AESEncryptionDecryption.java
@@ -0,0 +1,62 @@
+package com.demo.newsapi.encryption;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+import static javax.crypto.Cipher.ENCRYPT_MODE;
+import static javax.crypto.Cipher.DECRYPT_MODE;
+import static  java.nio.charset.StandardCharsets.UTF_8;
+
+/*
+ *Provides encryption and decryption of a string based on the secretKey provided
+ */
+@Slf4j
+public class AESEncryptionDecryption {
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+    private static final String ALGORITHM = "AES";
+
+    public static void prepareSecreteKey(String myKey) {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes(UTF_8);
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, ALGORITHM);
+        } catch (NoSuchAlgorithmException e) {
+            log.error("Error in prepareSecreteKey: " + e.toString());
+        }
+    }
+
+    public static String encrypt(String strToEncrypt, String secret) {
+        try {
+            prepareSecreteKey(secret);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        } catch (Exception e) {
+            log.error("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    public static String decrypt(String strToDecrypt, String secret) {
+        try {
+            prepareSecreteKey(secret);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } catch (Exception e) {
+            log.error("Error while decrypting: " + e.toString());
+        }
+        return null;
+    }
+}
